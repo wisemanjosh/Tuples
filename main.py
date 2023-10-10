ba8a8980d73ecd53be55dfb63cad0d6c7910d82b
JosueLessie <joslessie@hotmail.com>
2023-10-10 21:45:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Define a tuple coordinates with the latitude and longitude of London (51.5074, -0.1278).
+# Attempt to change one of the values in the coordinates tuple and observe the result.
+
+coordinates = (51.5074, -0.1278)
+
+coordinates = (-0.1278, 51.5074)
+
+print(coordinates)
